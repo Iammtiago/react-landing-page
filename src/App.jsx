a412ee32050f73440ede37b0fd1770881b9ee981
Tiago <santiagoAndres79@outlook.es>
2024-05-24 21:32:39 -0400

    new styles

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,8 @@ const App = () => {
     <div>
       <Navigation />
       <Header data={landingPageData.Header} />
-      <About data={landingPageData.About} />
       <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
